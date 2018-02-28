9e53b414f1db68f321dc81d634bb1ada74cc03c2
Tommi Teistela <totateis@gmail.com>
2018-02-28 22:53:38 +0200

    Make Finnish times of day less confusing

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import 'regenerator-runtime/runtime';
 import 'moment/locale/en-gb';
 import 'moment/locale/fi';
 
+import moment from 'moment';
+
 import './index.scss';
 
 import React from 'react';
@@ -17,6 +19,14 @@ import {
     Footer,
     // Breadcrumbs,
 } from 'src/layout';
+
+// Make the Finnish locale less confusing (is 15.10 a date or a time of day?)
+moment.updateLocale('fi', {
+    longDateFormat: {
+        LT: 'HH:mm',
+        LTS: 'HH:mm:ss',
+    } as any,
+});
 
 // Provide Webpack build id in the window env
 (window as any).BUILD_ID = process.env.BUILD_ID;
---
+++ src/views/Event/EventCompetition/CompetitionEnter/index.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import { action, runInAction } from 'mobx';
+
+import globalState from 'src/state';
+import { Form, FormGroup, L } from 'src/common';
+import { ICompetition } from 'src/api/interfaces';
+import { FormStore } from 'src/stores';
+import EventInfo from 'src/state/EventInfo';
+
+
+@observer
+export default class CompetitionEnter extends React.Component<{
+    eventInfo: EventInfo;
+    competition: ICompetition;
+}> {
+    form = new FormStore({
+        name: '',
+        creator: '',
+        description: '',
+        entryfile: null as File | null,
+        imagefile_original: null as File | null,
+        sourcefile: null as File | null,
+    }, (formStore) => {
+        return globalState.api.userCompoEntries.create({
+            ...formStore.toJS(),
+            compo: this.props.competition.id,
+        });
+    });
+
+    @action.bound
+    handleSubmit(event) {
+        this.form.submit().then(
+            (success) => runInAction(() => {
+                console.info('success:', success);
+                this.props.eventInfo.myEntries.refresh();
+                // FIXME: Get back to the compo page and make sure it refreshes.
+            }),
+            (error) => runInAction(() => {
+                console.error(error);
+                this.form.setError(error);
+            }),
+        );
+    }
+
+    render() {
+        const { form } = this;
+        return (
+            <Form form={form} onSubmit={this.handleSubmit}>
+                <h2><L text="entry.add" /></h2>
+                <FormGroup
+                    label={<L text="data.entry.name.title" />}
+                    help={<L text="data.entry.name.help" />}
+                    name="name"
+                />
+                <FormGroup
+                    label={<L text="data.entry.description.title" />}
+                    help={<L text="data.entry.description.help" />}
+                    name="description"
+                    input="textarea"
+                    lines={5}
+                />
+                <FormGroup
+                    label={<L text="data.entry.creator.title" />}
+                    help={<L text="data.entry.creator.help" />}
+                    name="creator"
+                />
+                <FormGroup
+                    label={<L text="data.entry.entryfile.title" />}
+                    help={<L text="data.entry.entryfile.help" />}
+                    name="entryfile"
+                    type="file"
+                />
+                <FormGroup
+                    label={<L text="data.entry.sourcefile.title" />}
+                    help={<L text="data.entry.sourcefile.help" />}
+                    name="sourcefile"
+                    type="file"
+                />
+                <FormGroup
+                    label={<L text="data.entry.imagefile_original.title" />}
+                    help={<L text="data.entry.imagefile_original.help" />}
+                    name="imagefile_original"
+                    type="file"
+                />
+                <div>
+                    <button className="btn btn-primary" disabled={this.form.isPending}>
+                        <L text="common.submit" />
+                    </button>
+                </div>
+            </Form>
+        );
+    }
+}
+
