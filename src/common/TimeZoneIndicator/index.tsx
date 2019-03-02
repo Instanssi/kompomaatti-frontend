8a82b0f6a5e182bf734af909b3fc52594c6561fa
Tommi Teistela <totateis@gmail.com>
2019-03-02 15:17:49 +0200

    Add time zone indicator
    It'll be more useful when the time zone can be changed.

--- src/common/TimeZoneIndicator/index.tsx
+++ src/common/TimeZoneIndicator/index.tsx
@@ -3,6 +3,17 @@ import { computed } from 'mobx';
 import { observer } from 'mobx-react';
 import moment from 'moment';
 
+import L from '../L';
+
+import './timezone.scss';
+
+/**
+ * Shows the UTC offset used to display times-of-day for a specific date.
+ *
+ * The date is required because of daylight savings time.
+ *
+ * @todo Allow switching the time zone to the party time via this?
+ */
 @observer
 export class TimeZoneIndicator extends React.Component<{
     time: string | number | moment.Moment | Date | null;
@@ -25,7 +36,7 @@ export class TimeZoneIndicator extends React.Component<{
         }
         const offsetStr = offsetHours >= 0 ? `+${offsetHours}` : `${offsetHours}`;
         return (
-            <span className="timezone-indicator">
+            <span className="timezone-indicator" title={L.getText('timeZone.yourTZ')}>
                 <span className="fa fa-clock-o" />
                 {' '}
                 UTC{offsetStr}
