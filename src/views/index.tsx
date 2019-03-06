d2aaea2c3c33701bd7212f4f768d1fc879a2cb3e
Tommi Teistela <totateis@gmail.com>
2019-03-06 23:55:26 +0200

    Handle unforeseen consequences at the view root level

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -5,10 +5,11 @@ import { Switch, Route } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { RouteComponentProps, withRouter } from 'react-router';
 
+import ErrorBoundary from 'src/common/ErrorBoundary';
+
 import FrontPage from './FrontPage';
 import EventsList from './EventsList';
 import Event from './Event';
-
 
 export class Views extends React.Component<RouteComponentProps<any>> {
     render() {
@@ -20,15 +21,17 @@ export class Views extends React.Component<RouteComponentProps<any>> {
 
         return (
             <div id="views">
-                <TransitionGroup className="transition-group">
-                    <CSSTransition key={locationKey} classNames="route" timeout={300}>
-                        <Switch location={location}>
-                            <Route exact path="/events"><EventsList /></Route>
-                            <Route path="/:eventId"><Event /></Route>
-                            <Route><FrontPage /></Route>
-                        </Switch>
-                    </CSSTransition>
-                </TransitionGroup>
+                <ErrorBoundary>
+                    <TransitionGroup className="transition-group">
+                        <CSSTransition key={locationKey} classNames="route" timeout={300}>
+                            <Switch location={location}>
+                                <Route exact path="/events"><EventsList /></Route>
+                                <Route path="/:eventId"><Event /></Route>
+                                <Route><FrontPage /></Route>
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                </ErrorBoundary>
             </div>
         );
     }
